3cd9bd2b418abdaa5a751afae8877a7466ed14b6
On0zday <on.0zday@gmail.com>
2023-03-21 10:40:00 -0300

    algoritimo

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+// selicionar inputs 
+const inputsTLeft = document.querySelector('input[name="top-left"]');
+const inputsBLeft = document.querySelector('input[name="bottom-left"]');
+
+
+const inputsTRigth = document.querySelector('input[name="top-rigth"]');
+const inputsBRigth = document.querySelector('input[name="bottom-rigth"]');
+
+// selicionar a caixa
+const box = document.getElementById("box");
+
+function convert (value) {  
+
+    const convert = parseInt(value);
+    return convert;
+}
+
+inputsTLeft.addEventListener("keyup", (e) => {
+
+    box.style.borderTopLeftRadius = `${parseInt(inputsTLeft.value)}px`;
+
+});
+
+
+inputsBLeft.addEventListener("keyup", (e) => {
+
+    box.style.borderBottomLeftRadius = `${parseInt(inputsTLeft.value)}px`;
+
+});
+
+
+inputsTRigth.addEventListener("keyup", (e) => {
+
+    box.style.borderTopRightRadius = `${parseInt(inputsTLeft.value)}px`;
+
+});
+
+
+
+inputsBRigth.addEventListener("keyup", (e) => {
+
+    box.style.borderBottomRightRadius = `${parseInt(inputsTLeft.value)}px`;
+
+});
